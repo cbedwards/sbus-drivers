79219f570065297601b61492b461b1fe46407fa3
Collin Edwards <edwards.evoeco@gmail.com>
2022-12-20 12:40:39 -0500

    Updated: funs.R contains the functions, drivers-example calls those functions for example cases.

--- 3_scripts/funs.R
+++ 3_scripts/funs.R
@@ -1,54 +1,9 @@
-## NOTE: it appears that each of our annual tifs are actually identical? Waiting for Naresh answer.
-
-## sandbox
 library(here)
 library(sp)
 library(raster)
 library(tidyverse)
 library(data.table)
 library(geosphere)
-
-mygrid <- raster(here("1_raw_data/precip/annual/annualPCP_1990.tif"))
-plot(mygrid)
-boston = cbind(-71.06, 42.36)
-davis = cbind(-121.74, 38.54)
-locs = rbind(boston, davis)
-points(x = boston[1], y = boston[2])
-df <- raster::extract(mygrid, locs);
-df
-
-mygrid <- raster(here("1_raw_data/precip/annual/annualPCP_2020.tif"))
-plot(mygrid)
-boston = cbind(-71.06, 42.36)
-davis = cbind(-121.74, 38.54)
-locs = rbind(boston, davis)
-points(x = boston[1], y = boston[2])
-df <- raster::extract(mygrid, locs);
-df
-
-length(df)
-
-
-## goals:
-##  for a given spatial extent, find the yearly (or other time frame) means for the given metric
-##  return this as a dataframe that can be saved or analyzed.
-
-## important first step: how does extract work with polygons?
-
-x1 = rbind(c(-80, 40), c(-80, 35), c(-85, 35), c(-85, 40))
-x2 = rbind(c(-82, 38), c(-82, 33), c(-87, 33), c(-87, 38))
-a = spPolygons(x1, x2)
-b = spPolygons(list(x1, x2))
-plot(mygrid)
-# plot(a, add = T)
-plot(b, add = T, col = 'coral')
-
-df <- raster::extract(mygrid, b)[[1]]
-
-## quick check of numerics:
-sum(df)/length(df)
-mean(df)
-
 
 ## Function(s) to define region of interest:
 ##  option 1: polygon from range map
@@ -85,11 +40,11 @@ make_survey_poly = function(code,
                              b = 45+90,
                              d = dist.m)
   lwr.left = destPointRhumb(p = pts,
-                             b = 45+90+90,
-                             d = dist.m)
-  up.left = destPointRhumb(p = pts,
-                            b = 45+90+90+90,
+                            b = 45+90+90,
                             d = dist.m)
+  up.left = destPointRhumb(p = pts,
+                           b = 45+90+90+90,
+                           d = dist.m)
   #going to do some fiddly apply work to turn these into lists of corners
   all.corners = cbind(up.right, lwr.right, lwr.left, up.left)
   #function to take a row and turn it into a dataframe of lons and lats
@@ -99,21 +54,10 @@ make_survey_poly = function(code,
   return(res)
 }
 
-## checking that it behaves decently:
-
-cabbage.poly = make_survey_poly("PIERAP", width = 50)
-plot(mygrid)
-plot(cabbage.poly, add = T, col = 'coral')
-
-
-
-## Function to take spatial extent, metric, and temporal resolution, and return a dataframe with a col for the average metric
-## across the space (using raster::extract), the temporal identifier, and the number of cells involved. For option 4,
-## return each point separately for each year.
-
 get_drivers = function(space, # points or polygon(s)
                        driver, #tmax, tmin, precip
-                       temporal.res){ #currently only annual
+                       temporal.res, #currently only annual
+                       verbose = FALSE){ #if true, print each file name when loading it
   ## Check that arguments are reasonable.
   stopifnot(temporal.res %in% c("annual"))
   stopifnot(driver %in% c("precip", "tmax", "tmin"))
@@ -131,7 +75,7 @@ get_drivers = function(space, # points or polygon(s)
       # grab temporal identifier from name
       temporal.id = strsplit(files.list[i], "_")[[1]][2]
       temporal.id = gsub("[.]tif", "", temporal.id)
-      print(files.list[i])
+      if(verbose){print(files.list[i])}
       mygrid <- raster(paste0(files.path, "/", files.list[i]))
       vals = unlist(raster::extract(mygrid, space))
       res$value[i] = mean(vals, na.rm = TRUE)
@@ -144,7 +88,7 @@ get_drivers = function(space, # points or polygon(s)
       # grab temporal identifier from name
       temporal.id = strsplit(files.list[i], "_")[[1]][2]
       temporal.id = gsub("[.]tif", "", temporal.id)
-      print(files.list[i])
+      if(verbose){print(files.list[i])}
       mygrid <- raster(paste0(files.path, "/", files.list[i]))
       vals = raster::extract(mygrid, space)
       df.cur = data.frame(lon = space[,1],
@@ -159,47 +103,3 @@ get_drivers = function(space, # points or polygon(s)
   return(res)
 }
 
-
-## example: extract time points from
-pts.cur = get_survey_points("PIERAP")
-df.drivers = get_drivers(space = pts.cur$mat,
-                         driver = "tmin",
-                         temporal.res = "annual")
-df.drivers$year = as.numeric(df.drivers$temporal.id)
-ggplot(df.drivers, aes(x = year, y = value)) +
-  geom_point()+
-  geom_smooth()
-## getting -1E9: whats going on? May be missing values?
-df.drivers$value[df.drivers$value < -1e8] = NA
-
-ggplot(df.drivers, aes(x = year, y = value)) +
-  geom_point()+
-  geom_smooth()
-
-drivers.sum = df.drivers %>%
-  group_by(year) %>%
-  summarize(value = mean(value, na.rm = T))
-ggplot(drivers.sum) +
-  geom_path(aes(x = year, y = value))
-
-## PROBLEM
-temp = df.drivers[df.drivers$lon == df.drivers$lon[1] &
-                    df.drivers$lat == df.drivers$lat[1],]
-temp = na.omit(temp)
-
-## checking polygon version
-df.drivers = get_drivers(space = b,
-                         driver = "tmin",
-                         temporal.res = "annual")
-df.drivers$year = as.numeric(df.drivers$temporal.id)
-ggplot(df.drivers) +
-  geom_path(aes(x = year, y = value))
-
-
-## checking that we can get "data-coverage-to-average-climate" pipeline working
-BCB.poly = make_survey_poly(code = "EUPHPHA", width = 50)
-plot(mygrid)
-plot(BCB.poly, add = T, col = 'coral')
-df.drivers = get_drivers(space = BCB.poly,
-                         driver = "tmin",
-                         temporal.res = "annual")
